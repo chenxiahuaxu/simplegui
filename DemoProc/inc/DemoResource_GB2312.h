f546224270a7ad35fcc467d8028a05cd395f9f2f
Polarix <xuyulin91@163.com>
2019-04-30 22:13:12 +0800

    2019-04-30：
    更新消息提示框的函数定义，可以动态指定显示消息框的字体。
    订正了演示例程，增加了一些演示例程与SDK的交互接口，方便移植。
    增加了SGUI_Config.h文件，SimpleGUI的配置移动到此文件中（PS之前有这样的文件，但是为了宏定义全局不遗漏，移动到了工程中，但是很多人还是觉得有这样一个配置文件更好些）。

---
+++ DemoProc/inc/DemoResource_GB2312.h
@@ -0,0 +1,57 @@
+#ifndef __INCLUDE_DEMO_RESOURCE_UTF8_H__
+#define __INCLUDE_DEMO_RESOURCE_UTF8_H__
+
+#if defined ( __CC_ARM )
+	//#pragma	diag_suppress	177							// Disabled "declared but never referenced." warning.
+#endif
+
+
+/* Screen 1: Multiple lines text. */
+/* Start screen scroll text. */
+#define     DEMO_START_NOTICE           (\
+"  欢迎来到SimpleGUI演示工程，本工程用于演示SimpleGUI各API的显示效果、\
+使用方法以及运作机理，在演示过程中，您可以通过串口发送特定字节模拟键盘\
+输入与SimpleGUI演示工程进行交互，以实现与模拟器中类似的效果。每字节高\
+四位为控制键码，低四位为主键码。\n\
+  使用串口发送0x04(空格)开始演示，串口配置为115200/N/8/1。"\
+)
+
+/* Screen 2: List*/
+/* Demo list title. */
+#define     DEMO_LIST_TITLE             ("测试列表")
+/* Enumed list item choice type. */
+#define     DEMO_LIST_ITEM_NOTICE_TEXT  ("文字消息")
+#define     DEMO_LIST_ITEM_NOTICE_TIME  ("系统时间")
+#define     DEMO_LIST_ITEM_ENUM_VALUE1  ("枚举项1")
+#define     DEMO_LIST_ITEM_ENUM_VALUE2  ("枚举项2")
+#define     DEMO_LIST_ITEM_ENUM_VALUE3  ("枚举项3")
+
+
+/* List items data.                      Sign   ListText                            List item type           List value                 Decimal point           Enumed text list    */
+/*                                                                                                           init.  min.   max.         init.  min.   max.                          */
+#define     DEMO_LIST_ITEM_0            {0,     "简单列表项",                       LIST_ITEM_NORMAL,		{0,     0,     0     },    {0,     0,     0     },  NULL}
+#define     DEMO_LIST_ITEM_1			{1,     "枚举类型列表项",		            LIST_ITEM_ENUM,			{0,     0,     1     },    {0,     0,     0     },  s_arrszNoticeType}
+#define     DEMO_LIST_ITEM_2            {2,     "数字列表项",			            LIST_ITEM_DIGIT,		{0,     -50,   50    },    {0,     0,     3     },  NULL}
+#define     DEMO_LIST_ITEM_3			{3,     "带小数的数字列表项",               LIST_ITEM_DIGIT,		{1,     -50,   50    },    {2,     0,     5     },  NULL}
+#define     DEMO_LIST_ITEM_4            {4,     "超长文字的简单列表项",             LIST_ITEM_NORMAL,		{0,     0,     0     },    {0,     0,     0     },  NULL}
+#define     DEMO_LIST_ITEM_5            {5,     "编辑框",				            LIST_ITEM_NORMAL,		{0,     0,     0     },    {0,     0,     0     },  NULL}
+#define     DEMO_LIST_ITEM_6			{6,     "实时曲线",				            LIST_ITEM_NORMAL,		{0,     0,     0     },    {0,     0,     0     },  NULL}
+#define     DEMO_LIST_ITEM_7            {7,     "测试项目1",				        LIST_ITEM_NORMAL,		{0,     0,     0     },    {0,     0,     0     },  NULL}
+#define     DEMO_LIST_ITEM_8            {8,     "测试项目2",				        LIST_ITEM_ENUM,			{0,     0,     2     },    {0,     0,     0     },  s_arrszEnumedValue}
+#define     DEMO_LIST_ITEM_9			{9,     "测试项目3",				        LIST_ITEM_DIGIT,		{0,     -50,   50    },    {0,     0,     3     },  NULL}
+
+/* Screen 3: Text notice box. */
+/* List notice text format. */
+#define     DEMO_LIST_NOTICE_TEXT_FMT   ("选择列表项%lu.")
+
+/* Screen 4: RTC notice box. */
+#define     DEMO_RTC_NOTICE_TEXT_FMT    ("系统时间\n%04u-%02u-%02u\n%02u:%02u:%02u")
+
+/* Screen 5: Variable box. */
+#define     DEMO_VARIABLE_BOX_TITLE     ("数值/文本编辑演示")
+#define     DEMO_VARIABLE_BOX_HELPER    ("TAB键切换焦点编辑框。\n上下箭头调整数值。\n左右箭头调整焦点字符。\n按空格键继续。")
+
+/* Others */
+#define     DEMO_TEST_TEXT              ("测试文字")
+
+#endif // __INCLUDE_DEMO_RESOURCE_UTF8_H__
--- GUI/src/SGUI_Notice.c
+++ GUI/src/SGUI_Notice.c
@@ -15,27 +15,26 @@
 //= User Macro definition.											    =//
 //=======================================================================//
 // User settings
-#define NOTICE_FONT_SIZE						(SGUI_FONT_SIZE_H12)
-#define NOTICE_BOX_EDGE_DIST					(6)
-#define NOTICE_BOX_MARGIN						(2)
+#define NOTICE_BOX_EDGE_DIST								(6)
+#define NOTICE_BOX_MARGIN									(2)
 
 // Automatic calculation
-#define NOTICE_BOX_WIDTH(SCR_OBJ)				(((SCR_OBJ).stSize.Width)-NOTICE_BOX_EDGE_DIST*2)
-#define NOTICE_BOX_HEIGHT_MIN					(g_stFontSize[NOTICE_FONT_SIZE].Height*2+NOTICE_BOX_MARGIN*2)
-#define NOTICE_BOX_HEIGHT_MAX(SCR_OBJ)			(((SCR_OBJ).stSize.Height)-NOTICE_BOX_EDGE_DIST*2)
-#define NOTICE_BOX_HEIGHT(LINES)				(LINES*g_stFontSize[NOTICE_FONT_SIZE].Height+NOTICE_BOX_MARGIN*2)
-#define NOTICE_BOX_POSX							(NOTICE_BOX_EDGE_DIST)
-#define NOTICE_BOX_POSY(SCR_OBJ, HEIGHT)		((((SCR_OBJ).stSize.Height)-HEIGHT)/2)
-#define NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)	(NOTICE_BOX_WIDTH(SCR_OBJ)-NOTICE_BOX_MARGIN*4)
-#define	NOTICE_TEXT_AREA_WIDTH(SCR_OBJ)			(NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)-NOTICE_ICON_SIZE-NOTICE_BOX_MARGIN*2)
-#define NOTICE_TEXT_AREA_HEIGHT(LINES)			(NOTICE_BOX_HEIGHT(LINES)-NOTICE_BOX_MARGIN*2)
-#define NOTICE_TEXT_AREA_HEIGHT_MIN				(g_stFontSize[NOTICE_FONT_SIZE].Height*2)
-#define NOTICE_TEXT_AREA_HEIGHT_MAX(SCR_OBJ)	(NOTICE_BOX_HEIGHT_MAX(SCR_OBJ)-NOTICE_BOX_MARGIN*2)
-#define NOTICE_TEXT_POSX_NOICON					(NOTICE_BOX_POSX+NOTICE_BOX_MARGIN*2)
-#define NOTICE_TEXT_POSX						(NOTICE_TEXT_POSX_NOICON+NOTICE_ICON_SIZE+NOTICE_BOX_MARGIN*2)
-#define NOTICE_TEXT_POSY(SCR_OBJ, HEIGHT)		(NOTICE_BOX_POSY(SCR_OBJ, HEIGHT)+NOTICE_BOX_MARGIN)
-#define	NOTICE_TEXT_LINES_MAX(SCR_OBJ)			(NOTICE_TEXT_AREA_WIDTH(SCR_OBJ)/g_stFontSize[NOTICE_FONT_SIZE].Width)
-#define	NOTICE_TEXT_LINES_MAX_NOICON(SCR_OBJ)	(NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)/g_stFontSize[NOTICE_FONT_SIZE].Width)
+#define NOTICE_BOX_WIDTH(SCR_OBJ)							(((SCR_OBJ).stSize.Width)-NOTICE_BOX_EDGE_DIST*2)
+#define NOTICE_BOX_HEIGHT_MIN(FONT_SIZE)					(g_stFontSize[FONT_SIZE].Height*2+NOTICE_BOX_MARGIN*2)
+#define NOTICE_BOX_HEIGHT_MAX(SCR_OBJ)						(((SCR_OBJ).stSize.Height)-NOTICE_BOX_EDGE_DIST*2)
+#define NOTICE_BOX_HEIGHT(LINES, FONT_SIZE)					(LINES*g_stFontSize[FONT_SIZE].Height+NOTICE_BOX_MARGIN*2)
+#define NOTICE_BOX_POSX										(NOTICE_BOX_EDGE_DIST)
+#define NOTICE_BOX_POSY(SCR_OBJ, HEIGHT)					((((SCR_OBJ).stSize.Height)-HEIGHT)/2)
+#define NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)				(NOTICE_BOX_WIDTH(SCR_OBJ)-NOTICE_BOX_MARGIN*4)
+#define	NOTICE_TEXT_AREA_WIDTH(SCR_OBJ)						(NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)-NOTICE_ICON_SIZE-NOTICE_BOX_MARGIN*2)
+#define NOTICE_TEXT_AREA_HEIGHT(LINES, FONT_SIZE)			(NOTICE_BOX_HEIGHT(LINES, FONT_SIZE)-NOTICE_BOX_MARGIN*2)
+#define NOTICE_TEXT_AREA_HEIGHT_MIN(FONT_SIZE)				(g_stFontSize[FONT_SIZE].Height*2)
+#define NOTICE_TEXT_AREA_HEIGHT_MAX(SCR_OBJ)				(NOTICE_BOX_HEIGHT_MAX(SCR_OBJ)-NOTICE_BOX_MARGIN*2)
+#define NOTICE_TEXT_POSX_NOICON								(NOTICE_BOX_POSX+NOTICE_BOX_MARGIN*2)
+#define NOTICE_TEXT_POSX									(NOTICE_TEXT_POSX_NOICON+NOTICE_ICON_SIZE+NOTICE_BOX_MARGIN*2)
+#define NOTICE_TEXT_POSY(SCR_OBJ, HEIGHT)					(NOTICE_BOX_POSY(SCR_OBJ, HEIGHT)+NOTICE_BOX_MARGIN)
+#define	NOTICE_TEXT_LINES_MAX(SCR_OBJ, FONT_SIZE)			(NOTICE_TEXT_AREA_WIDTH(SCR_OBJ)/g_stFontSize[FONT_SIZE].Width)
+#define	NOTICE_TEXT_LINES_MAX_NOICON(SCR_OBJ, FONT_SIZE)	(NOTICE_TEXT_AREA_WIDTH_NOICON(SCR_OBJ)/g_stFontSize[FONT_SIZE].Width)
 
 //=======================================================================//
 //= Function define.										            =//
@@ -47,12 +46,13 @@
 /** Params:																**/
 /**	@ pstIFObj[in]:		SimpleGUI object pointer.						**/
 /**	@ szNoticeText[in]:	Notice text.									**/
+/** @ iFontSize[in]:	Text font size.									**/
 /**	@ uiTextOffset[in]:	Text top offset.								**/
 /**	@ eIcon[in]:		Notice icon index.								**/
 /** Return:			Remaining text height display.						**/
 /** Notice:			None.												**/
 /*************************************************************************/
-SGUI_SIZE SGUI_Notice_Repaint(SGUI_SCR_DEV* pstIFObj, SGUI_CSZSTR szNoticeText, SGUI_INT uiTextOffset, SGUI_NOTICE_ICON_IDX eIcon)
+SGUI_SIZE SGUI_Notice_Repaint(SGUI_SCR_DEV* pstIFObj, SGUI_CSZSTR szNoticeText, SGUI_FONT_SIZE iFontSize, SGUI_INT uiTextOffset, SGUI_NOTICE_ICON_IDX eIcon)
 {
 	/*----------------------------------*/
 	/* Variable Declaration				*/
@@ -72,17 +72,17 @@ SGUI_SIZE SGUI_Notice_Repaint(SGUI_SCR_DEV* pstIFObj, SGUI_CSZSTR szNoticeText, 
 	// Get max line of notice text.
 	if(SGUI_ICON_NONE != eIcon)
 	{
-		uiLineCount = SGUI_Text_GetMultiLineTextLines(pszNoticeTextPtr, NOTICE_TEXT_LINES_MAX(*pstIFObj));
+		uiLineCount = SGUI_Text_GetMultiLineTextLines(pszNoticeTextPtr, NOTICE_TEXT_LINES_MAX(*pstIFObj, iFontSize));
 	}
 	else
 	{
-		uiLineCount = SGUI_Text_GetMultiLineTextLines(pszNoticeTextPtr, NOTICE_TEXT_LINES_MAX_NOICON(*pstIFObj));
+		uiLineCount = SGUI_Text_GetMultiLineTextLines(pszNoticeTextPtr, NOTICE_TEXT_LINES_MAX_NOICON(*pstIFObj, iFontSize));
 	}
 	if(uiLineCount < 2)
 	{
 		uiLineCount = 2;
 	}
-	uiNoticeBoxHeight = NOTICE_BOX_HEIGHT(uiLineCount);
+	uiNoticeBoxHeight = NOTICE_BOX_HEIGHT(uiLineCount, iFontSize);
 	if(uiNoticeBoxHeight > NOTICE_BOX_HEIGHT_MAX(*pstIFObj))
 	{
 		uiNoticeBoxHeight = NOTICE_BOX_HEIGHT_MAX(*pstIFObj);
@@ -114,13 +114,13 @@ SGUI_SIZE SGUI_Notice_Repaint(SGUI_SCR_DEV* pstIFObj, SGUI_CSZSTR szNoticeText, 
 		stTextDisplayArea.Width = NOTICE_TEXT_AREA_WIDTH_NOICON(*pstIFObj);
 	}
 	stTextDisplayArea.PosY = NOTICE_TEXT_POSY(*pstIFObj, uiNoticeBoxHeight);
-	stTextDisplayArea.Height = NOTICE_TEXT_AREA_HEIGHT(uiLineCount);
+	stTextDisplayArea.Height = NOTICE_TEXT_AREA_HEIGHT(uiLineCount, iFontSize);
     if(stTextDisplayArea.Height > NOTICE_TEXT_AREA_HEIGHT_MAX(*pstIFObj))
 	{
 		stTextDisplayArea.Height = NOTICE_TEXT_AREA_HEIGHT_MAX(*pstIFObj);
 	}
 
-    uiTextLines = SGUI_Text_DrawMultipleLinesText(pstIFObj, pszNoticeTextPtr, NOTICE_FONT_SIZE, &stTextDisplayArea, uiTextOffset, SGUI_DRAW_NORMAL);
+    uiTextLines = SGUI_Text_DrawMultipleLinesText(pstIFObj, pszNoticeTextPtr, iFontSize, &stTextDisplayArea, uiTextOffset, SGUI_DRAW_NORMAL);
 
     return uiTextLines;
 }
